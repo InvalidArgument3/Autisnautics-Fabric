4b4ebd24c252a3c790b2a271431517b376948d7e
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-02 05:39:18 -0500

    questing intensifies todo: replacelead

---
+++ .minecraft/kubejs/server_scripts/fixgunrecipes.js
@@ -0,0 +1,12 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! loaded gunscripts)')
+
+ServerEvents.recipes(event => {
+	
+	  event.remove({ output: 'anim_guns:enriched_iron' }) // Removes recipe by ID. in this case, data/minecraft/recipes/glowstone.json
+
+
+})
