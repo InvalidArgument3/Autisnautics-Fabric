486ae696a494a6cf9e5e4acb64a1e54fb96b5586
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-02 23:20:42 -0500

    few updates

--- .minecraft/kubejs/server_scripts/fixgunrecipes.js
+++ .minecraft/kubejs/server_scripts/fixgunrecipes.js
@@ -6,7 +6,14 @@ console.info('Hello, World! loaded gunscripts)')
 
 ServerEvents.recipes(event => {
 	
-	  event.remove({ output: 'anim_guns:enriched_iron' }) // Removes recipe by ID. in this case, data/minecraft/recipes/glowstone.json
+	event.remove({ output: 'anim_guns:enriched_iron' }) // Removes recipe by ID. in this case, data/minecraft/recipes/glowstone.json
+	event.remove({ output: 'anim_guns:hardened_iron_ingot' }) 
+	event.remove({ output: 'anim_guns:hardened_iron_nugget' });
+	event.remove({ output: 'anim_guns:enriched_iron_nugget' });
+	  
+	event.replaceInput({}, 'anim_guns:hardened_iron_ingot', 'techreborn:steel_ingot');
+
+	event.replaceInput({}, /anim_guns:.*iron.*/, 'techreborn:lead_ingot');
 
 
 })
