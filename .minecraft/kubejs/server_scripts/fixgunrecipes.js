44a3afc8ed471165b707f2776f32fd0604e54eb6
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-04 04:34:14 -0500

    mem

--- .minecraft/kubejs/server_scripts/fixgunrecipes.js
+++ .minecraft/kubejs/server_scripts/fixgunrecipes.js
@@ -20,20 +20,62 @@ ServerEvents.recipes(event => {
 	event.remove({ output: 'anim_guns:shotgun_shell' });
 	
 	
+	event.remove({ output: 'anim_guns:blueprint_bundle' });
 	
-event.shaped(
+
+	
+
+
+event.shapeless(
   Item.of('anim_guns:standard_handgun_cartridge', 8), // arg 1: output
   [ 
-    'A', 
-    'B ', // arg 2: the shape (array of strings)
-    'C'  
-  ],
-  {
-    A: 'techreborn:lead_nugget', 
-    B: 'minecraft:gunpowder',  //arg 3: the mapping object
-    C: 'minecraft:lead_ingot'   
-  }
+    'techreborn:lead_nugget',
+    'minecraft:gunpowder', 	//arg 2: the array of inputs
+    'create:brass_ingot'
+  ]
+)
+
+event.shapeless(
+  Item.of('anim_guns:heavy_handgun_cartridge', 4), // arg 1: output
+  [ 
+    'techreborn:steel_nugget',
+    'minecraft:gunpowder', 	//arg 2: the array of inputs
+    'create:brass_ingot'
+  ]
+)
+
+event.shapeless(
+  Item.of('anim_guns:standard_rifle_cartridge', 4), // arg 1: output
+  [ 
+    'techreborn:lead_ingot',
+    'minecraft:gunpowder', 	//arg 2: the array of inputs
+    'create:brass_ingot'
+  ]
+)
+
+event.shapeless(
+  Item.of('anim_guns:heavy_rifle_cartridge', 8), // arg 1: output
+  [ 
+    'techreborn:steel_ingot',
+    'minecraft:gunpowder', 	//arg 2: the array of inputs
+    'create:brass_ingot'
+  ]
+)
+
+event.shapeless(
+  Item.of('anim_guns:shotgun_shell', 4), // arg 1: output
+  [ 
+    '3x techreborn:lead_nugget',
+    'minecraft:gunpowder', 	//arg 2: the array of inputs
+    'minecraft:paper'
+  ]
 )
 
 
+
 })
+
+
+	
+
+  
